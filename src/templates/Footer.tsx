c55270becc0bfd72e528a73a68c0468519b6ce74
tak n <tkn.infomail@gmail.com>
2022-06-25 15:20:45 +0900

    Update opensea

--- src/templates/Footer.tsx
+++ src/templates/Footer.tsx
@@ -1,15 +1,16 @@
 import Link from 'next/link';
-
-import { Background } from '../background/Background';
-import { CenteredFooter } from '../footer/CenteredFooter';
-import { Section } from '../layout/Section';
-import { Logo } from './Logo';
 import {
   FaHome,
   FaTwitter,
   FaYoutube,
   FaInstagramSquare,
 } from 'react-icons/fa';
+import { GiSailboat } from 'react-icons/gi';
+
+import { Background } from '../background/Background';
+import { CenteredFooter } from '../footer/CenteredFooter';
+import { Section } from '../layout/Section';
+import { Logo } from './Logo';
 
 function Footer() {
   return (
@@ -17,45 +18,57 @@ function Footer() {
       <Section>
         <CenteredFooter
           logo={<Logo />}
-          iconList={<>
-            <Link href="https://nuovotaka.com">
-              <a>
-                <FaHome
-                  className="inline-block h-10 w-10 fill-current"
-                  color={'#222'} />
-              </a>
-            </Link>
+          iconList={
+            <>
+              <Link href="https://nuovotaka.com">
+                <a>
+                  <FaHome
+                    className="inline-block h-10 w-10 fill-current"
+                    color={'#222'}
+                  />
+                </a>
+              </Link>
 
-            <Link href="https://twitter.com/nuovotaka">
-              <a>
-                <FaTwitter
-                  className="inline-block h-10 w-10 fill-current"
-                  color={'#222'}
-                />
-              </a>
-            </Link>
+              <Link href="https://twitter.com/nuovotaka">
+                <a>
+                  <FaTwitter
+                    className="inline-block h-10 w-10 fill-current"
+                    color={'#222'}
+                  />
+                </a>
+              </Link>
 
-            <Link href="https://www.youtube.com/channel/UCIGPyA3GEOTn1F9-kb8qfAg/featured">
-              <a>
-                <FaYoutube
-                  className="inline-block h-10 w-10 fill-current"
-                  color={'#222'}
-                />
-              </a>
-            </Link>
+              <Link href="https://www.youtube.com/channel/UCIGPyA3GEOTn1F9-kb8qfAg/featured">
+                <a>
+                  <FaYoutube
+                    className="inline-block h-10 w-10 fill-current"
+                    color={'#222'}
+                  />
+                </a>
+              </Link>
 
-            <Link href="https://www.instagram.com/takahiro_723/">
-              <a>
-                <FaInstagramSquare
-                  className="inline-block h-10 w-10 fill-current"
-                  color={'#222'}
-                />
-              </a>
-            </Link>
+              <Link href="https://www.instagram.com/takahiro_723/">
+                <a>
+                  <FaInstagramSquare
+                    className="inline-block h-10 w-10 fill-current"
+                    color={'#222'}
+                  />
+                </a>
+              </Link>
 
-          </>}
-        >
-        </CenteredFooter>
+              <Link href="https://www.instagram.com/takahiro_723/">
+                <a>
+                  <GiSailboat
+                    className="inline-block h-10 w-10 fill-current"
+                    color={'#222'}
+                  />
+                </a>
+              </Link>
+            </>
+          }
+          // eslint-disable-next-line react/no-children-prop
+          children={undefined}
+        ></CenteredFooter>
       </Section>
     </Background>
   );
