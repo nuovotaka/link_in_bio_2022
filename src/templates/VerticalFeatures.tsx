e24a4678c5c98c0b7a99b21eb75d1cffe2777676
tak n <tkn.infomail@gmail.com>
2022-07-01 10:44:42 +0900

    change sns icon

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -1,3 +1,11 @@
+import {
+  FaHome,
+  FaInstagramSquare,
+  FaTwitter,
+  FaYoutube,
+} from 'react-icons/fa';
+import { GiSailboat } from 'react-icons/gi';
+
 import { VerticalFeatureRow } from '../feature/VerticalFeatureRow';
 import { Section } from '../layout/Section';
 
@@ -6,36 +14,31 @@ const VerticalFeatures = () => (
     <VerticalFeatureRow
       title="Web site"
       site_url="https://nuovotaka.com"
-      fa={'home'}
-      color={'#222'}
+      icon={<FaHome color={'#222'} size={'2.3rem'} />}
       bgcolor={'bg-white'}
     />
     <VerticalFeatureRow
       title="Twitter"
       site_url="https://twitter.com/nuovotaka"
-      fa={'twitter'}
-      color={'#1d9bf0'}
+      icon={<FaTwitter color={'#1d9bf0'} size={'2.3rem'} />}
       bgcolor={'bg-white'}
     />
     <VerticalFeatureRow
       title="YouTube"
       site_url="https://www.youtube.com/channel/UCIGPyA3GEOTn1F9-kb8qfAg/featured"
-      fa={'youtube'}
-      color={'#ff0100'}
+      icon={<FaYoutube color={'#ff0100'} size={'2.3rem'} />}
       bgcolor={'bg-white'}
     />
     <VerticalFeatureRow
       title="instagram"
       site_url="https://www.instagram.com/takahiro_723/"
-      fa={'instagram'}
-      color={'#d52a7e'}
+      icon={<FaInstagramSquare color={'#d52a7e'} size={'2.3rem'} />}
       bgcolor={'bg-white'}
     />
     <VerticalFeatureRow
       title="OpenSea"
       site_url="https://opensea.io/collection/t723hmm"
-      fa={'opensea'}
-      color={'#ffffff'}
+      icon={<GiSailboat color={'#ffffff'} size={'2.3rem'} />}
       bgcolor={'bg-blue-500'}
     />
   </Section>
